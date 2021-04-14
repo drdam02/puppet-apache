bc16c07d882c7fcecfe73a71c577a8d35f6b090a
root <root@cb3fd953491c.mylabserver.com>
2021-04-14 09:26:28 +0000

    Added service class

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,7 +6,18 @@
 #   include apache
 class apache (
   String $install_name,
-  String $install_ensure
+  String $install_ensure,
+  String $config_ensure,
+  String $config_path,
+  Enum['running','stopped'] $service_ensure,
+  String $service_name,
+  Boolean $service_enable,
 ) {
-  include apache::install
+  contain apache::install
+  contain apache::config
+  contain apache::service
+
+  Class['::apache::install']
+  -> Class['::apache::config']
+  ~> Class['::apache::service']
 }
